174734010b4d9e620765ad2f6e18d4ed0a67b640
Apoorva Joshi <apoorvaj@apoorvaj.io>
2023-01-08 21:48:47 +0100

    Quantize grids so that they connect to one another

--- Assets/v2/Runtime/Grid.cs
+++ Assets/v2/Runtime/Grid.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
@@ -6,13 +7,45 @@ public class Grid : MonoBehaviour
 
     void OnDrawGizmos()
     {
+        // Quantize the grid's position so that all grids line up with each other
+        float q = (Mathf.Sqrt(3f) / 3f * transform.position.x - 1f / 3f * transform.position.z) / CellV2.OuterRadius;
+        float r = (2f / 3f * transform.position.z) / CellV2.OuterRadius;
+        q = Mathf.Round(q);
+        r = Mathf.Round(r);
+
+        Vector3 pos = new Vector3(
+            CellV2.InnerRadius * (2f * q + r),
+            0f,
+            CellV2.OuterRadius * (1.5f * r)
+        );
+
         Vector2 size = new Vector2(
             CellV2.InnerRadius * 2f * (NumTiles.x + 0.5f),
             CellV2.OuterRadius * (2f + (NumTiles.y - 1) * 1.5f)
         );
-        Gizmos.DrawLine(transform.position, transform.position + new Vector3(size.x, 0f, 0f));
-        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0f, 0f, size.y));
-        Gizmos.DrawLine(transform.position + new Vector3(size.x, 0f, 0f), transform.position + new Vector3(size.x, 0f, size.y));
-        Gizmos.DrawLine(transform.position + new Vector3(0f, 0f, size.y), transform.position + new Vector3(size.x, 0f, size.y));
+        Gizmos.DrawLine(pos, pos + new Vector3(size.x, 0f, 0f));
+        Gizmos.DrawLine(pos, pos + new Vector3(0f, 0f, size.y));
+        Gizmos.DrawLine(pos + new Vector3(size.x, 0f, 0f), pos + new Vector3(size.x, 0f, size.y));
+        Gizmos.DrawLine(pos + new Vector3(0f, 0f, size.y), pos + new Vector3(size.x, 0f, size.y));
+
+        if (transform.hasChanged)
+        {
+            transform.hasChanged = false;
+            Vector3 offset = pos - transform.position;
+            CellV2[] cells = gameObject.GetComponentsInChildren<CellV2>();
+            foreach (CellV2 cell in cells)
+            {
+                String[] tokens = cell.name.Split('_');
+                Debug.Assert(tokens.Length == 3);
+                int x = int.Parse(tokens[1]);
+                int y = int.Parse(tokens[2]);
+                cell.transform.localPosition = offset +
+                new Vector3(
+                    (0.5f + x + y * 0.5f - y / 2) * CellV2.InnerRadius * 2f,
+                    0f,
+                    CellV2.OuterRadius + y * (CellV2.OuterRadius * 1.5f)
+                );
+            }
+        }
     }
 }
