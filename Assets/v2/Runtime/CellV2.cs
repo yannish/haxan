a87506c3cd517cca7a061fb833acb760f249238b
Apoorva Joshi <apoorvaj@apoorvaj.io>
2023-01-10 00:07:36 +0100

    Snap both grids and cells independently

--- Assets/v2/Runtime/CellV2.cs
+++ Assets/v2/Runtime/CellV2.cs
@@ -8,41 +8,46 @@ public class CellV2 : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
-        float3x3 cartesianToHex = math.mul(
-            new float3x3(
-                Mathf.Sqrt(3f) / (3f * OuterRadius), -1f / (3f * OuterRadius), 0f,
-                0f, 2f / (3f * OuterRadius), 0f,
-                0f, 0f, 1f
-            ),
-            new float3x3(
-                1f, 0f, -InnerRadius,
-                0f, 1f, -OuterRadius,
-                0f, 0f, 1f
-            )
-        );
-        float3 cartesian = new float3(transform.position.x, transform.position.z, 1f);
-        float2 hex = math.mul(cartesianToHex, cartesian).xy;
-        hex = math.round(hex);
-        float3x3 hexToCartesian = math.mul(
-            new float3x3(
-                1f, 0f, InnerRadius,
-                0f, 1f, OuterRadius,
-                0f, 0f, 1f
-            ),
-            new float3x3(
-                InnerRadius * 2f, InnerRadius, 0f,
-                0f, 1.5f * OuterRadius, 0f,
-                0f, 0f, 1f
-            )
-        );
-        float2 roundedCartesian = math.mul(hexToCartesian, new float3(hex.x, hex.y, 1f)).xy;
+        if (transform.hasChanged)
+        {
+            // TODO: Unify into a static matrix somewhere
+            float3x3 cartesianToHex = math.mul(
+                new float3x3(
+                    Mathf.Sqrt(3f) / (3f * OuterRadius), -1f / (3f * OuterRadius), 0f,
+                    0f, 2f / (3f * OuterRadius), 0f,
+                    0f, 0f, 1f
+                ),
+                new float3x3(
+                    1f, 0f, -InnerRadius,
+                    0f, 1f, -OuterRadius,
+                    0f, 0f, 1f
+                )
+            );
+            float3 cartesian = new float3(transform.localPosition.x, transform.localPosition.z, 1f);
+            float2 hex = math.mul(cartesianToHex, cartesian).xy;
+            hex = math.round(hex);
+            float3x3 hexToCartesian = math.mul(
+                new float3x3(
+                    1f, 0f, InnerRadius,
+                    0f, 1f, OuterRadius,
+                    0f, 0f, 1f
+                ),
+                new float3x3(
+                    InnerRadius * 2f, InnerRadius, 0f,
+                    0f, 1.5f * OuterRadius, 0f,
+                    0f, 0f, 1f
+                )
+            );
+            float2 roundedCartesian = math.mul(hexToCartesian, new float3(hex.x, hex.y, 1f)).xy;
 
-        Vector3 pos = new Vector3(
-            roundedCartesian.x,
-            0f,
-            roundedCartesian.y
-        );
+            Vector3 pos = new Vector3(
+                roundedCartesian.x,
+                0f,
+                roundedCartesian.y
+            );
 
-        transform.position = pos;
+            transform.localPosition = pos;
+            transform.hasChanged = false;
+        }
     }
 }
--- Assets/v2/Runtime/Grid.cs
+++ Assets/v2/Runtime/Grid.cs
@@ -1,4 +1,4 @@
-using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
@@ -7,45 +7,57 @@ public class Grid : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        // Quantize the grid's position so that all grids line up with each other
-        float q = (Mathf.Sqrt(3f) / 3f * transform.position.x - 1f / 3f * transform.position.z) / CellV2.OuterRadius;
-        float r = (2f / 3f * transform.position.z) / CellV2.OuterRadius;
-        q = Mathf.Round(q);
-        r = Mathf.Round(r);
-
-        Vector3 pos = new Vector3(
-            CellV2.InnerRadius * (2f * q + r),
-            0f,
-            CellV2.OuterRadius * (1.5f * r)
-        );
-
-        Vector2 size = new Vector2(
-            CellV2.InnerRadius * 2f * (NumTiles.x + 0.5f),
-            CellV2.OuterRadius * (2f + (NumTiles.y - 1) * 1.5f)
-        );
-        Gizmos.DrawLine(pos, pos + new Vector3(size.x, 0f, 0f));
-        Gizmos.DrawLine(pos, pos + new Vector3(0f, 0f, size.y));
-        Gizmos.DrawLine(pos + new Vector3(size.x, 0f, 0f), pos + new Vector3(size.x, 0f, size.y));
-        Gizmos.DrawLine(pos + new Vector3(0f, 0f, size.y), pos + new Vector3(size.x, 0f, size.y));
-
         if (transform.hasChanged)
         {
+            float3x3 cartesianToHex = math.mul(
+                new float3x3(
+                    Mathf.Sqrt(3f) / (3f * CellV2.OuterRadius), -1f / (3f * CellV2.OuterRadius), 0f,
+                    0f, 2f / (3f * CellV2.OuterRadius), 0f,
+                    0f, 0f, 1f
+                ),
+                new float3x3(
+                    1f, 0f, 0f,
+                    0f, 1f, 0f,
+                    0f, 0f, 1f
+                )
+            );
+            float3 cartesian = new float3(transform.position.x, transform.position.z, 1f);
+            float2 hex = math.mul(cartesianToHex, cartesian).xy;
+            hex = math.round(hex);
+            float3x3 hexToCartesian = math.mul(
+                new float3x3(
+                    1f, 0f, 0f,
+                    0f, 1f, 0f,
+                    0f, 0f, 1f
+                ),
+                new float3x3(
+                    CellV2.InnerRadius * 2f, CellV2.InnerRadius, 0f,
+                    0f, 1.5f * CellV2.OuterRadius, 0f,
+                    0f, 0f, 1f
+                )
+            );
+            float2 roundedCartesian = math.mul(hexToCartesian, new float3(hex.x, hex.y, 1f)).xy;
+
+            Vector3 pos = new Vector3(
+                roundedCartesian.x,
+                0f,
+                roundedCartesian.y
+            );
+            transform.position = pos;
             transform.hasChanged = false;
-            Vector3 offset = pos - transform.position;
-            CellV2[] cells = gameObject.GetComponentsInChildren<CellV2>();
-            foreach (CellV2 cell in cells)
-            {
-                String[] tokens = cell.name.Split('_');
-                Debug.Assert(tokens.Length == 3);
-                int x = int.Parse(tokens[1]);
-                int y = int.Parse(tokens[2]);
-                cell.transform.localPosition = offset +
-                new Vector3(
-                    (0.5f + x + y * 0.5f - y / 2) * CellV2.InnerRadius * 2f,
-                    0f,
-                    CellV2.OuterRadius + y * (CellV2.OuterRadius * 1.5f)
-                );
-            }
+        }
+        // Draw the bounds gizmo
+        {
+            Vector3 pos = transform.position;
+            Vector2 size = new Vector2(
+                CellV2.InnerRadius * 2f * (NumTiles.x + 0.5f),
+                CellV2.OuterRadius * (2f + (NumTiles.y - 1) * 1.5f)
+            );
+
+            Gizmos.DrawLine(pos, pos + new Vector3(size.x, 0f, 0f));
+            Gizmos.DrawLine(pos, pos + new Vector3(0f, 0f, size.y));
+            Gizmos.DrawLine(pos + new Vector3(size.x, 0f, 0f), pos + new Vector3(size.x, 0f, size.y));
+            Gizmos.DrawLine(pos + new Vector3(0f, 0f, size.y), pos + new Vector3(size.x, 0f, size.y));
         }
     }
 }
